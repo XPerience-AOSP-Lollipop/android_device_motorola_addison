d32a815a592cb49dd61da738f008560cb11209dd
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-07-13 04:02:02 -0500

    addison: fix camera one permissions
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,9 +79,6 @@ BLUETOOTH_HCI_USE_MCT := true
 # Camera
 USE_DEVICE_SPECIFIC_CAMERA := true
 
-#Camera: allow to use motorola camera app
-MOTOCAMERA  := true
-
 # Charger
 BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGER_ENABLE_SUSPEND := true
