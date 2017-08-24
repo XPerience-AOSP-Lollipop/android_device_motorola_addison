2ea70a535c61cebe36ee3c3873e4920175d7af97
Alberto97 <albertop2197@gmail.com>
2017-08-24 14:08:54 +0200

    addison: Switch to TARGET_FS_CONFIG_GEN for fs_config
     * On Oreo, android_filesystem_config is generated by a python script
       that provides a nice way to add AIDs without overriding any header.
     * Switch AID values from 9xxx to 5xxx since accepted ranges
       for OEMs are only 5000-5999 and 2900-2999
     * Remove fs_config_files since it is already in embedded.mk
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -152,10 +152,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fingerprintd
 
-# For android_filesystem_config.h
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # GPS
 PRODUCT_PACKAGES += \
     libgnsspps \
