f553e6f2553a501f67fad04d2de6ddbb79b54904
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-11-12 22:28:02 -0600

    addison: add drm service
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- device.mk
+++ device.mk
@@ -144,6 +144,7 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-service.widevine
     libprotobuf-cpp-lite
 
