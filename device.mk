4c0ca5268acf7c41cb7ef084a836e21b9a832ff6
Alberto97 <albertop2197@gmail.com>
2017-12-10 14:09:02 +0100

    Bluetooth update for Oreo

--- device.mk
+++ device.mk
@@ -95,7 +95,6 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl \
     libbt-vendor
 
 # Camera
@@ -248,7 +247,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     init.mmi.touch.sh \
     wlan_carrier_bin.sh \
-    init.qcom.bt.sh \
     init.qcom.ril.sh
 
 PRODUCT_PACKAGES += \
