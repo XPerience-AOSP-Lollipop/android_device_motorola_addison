693315b1407a768c4ac67a849b09e3e4e7b244b7
klozz <klozz707@gmail.com>
2017-12-27 01:30:01 -0500

    Revert "addison: Use build variables to build device matrices."
    This reverts commit 4561955214e73096682a0be0c74e32cb2f8467e6.

--- device.mk
+++ device.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     gps.msm8953 \
     libshims_get_process_name
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
